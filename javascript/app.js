f67dc0a76fd8fa22d3a85a8613c0799b5d2012ef
Jake Haberle <jake.haberle@gmail.com>
2020-03-08 15:47:13 -0600

    functions added

--- javascript/app.js
+++ javascript/app.js
@@ -1,42 +1,38 @@
-
-
-var players = [];
-
-
-
+var players = ["Ichiro", "Barry Bonds", "Babe Ruth"];
 
 function newButton() {
-
-$('#new-player').empty();
-
-for (var i = 0; i < players.length; i++) {
-    var a = $('<button>');
-    a.addClass('player');
-    a.attr('data-name', players[i]);
-    $('#new-player').append(a);
-    console.log("I cliked a button");
+    
+    $('#new-player').empty();
+    for (var i = 0; i < players.length; i++) {
+        var addedPlayer = $('<button>');
+        addedPlayer.addClass('player');
+        addedPlayer.attr('data-name', players[i]);
+        $('#new-player').append(addedPlayer);
+        
+    }
 }
 
-}
+    $('search-player').on('click', function(event) {
+        event.preventDefault();
+        var player = $('#player-input').val().trim();
+        players.push(player);
 
-$('search-player').on('click', function(event) {
-    event.preventDefault();
-    var player = $('#player-input').val().trim();
-    players.push(player);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
-
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+        $('#new-player').text(JSON.stringify(response));
+        newButton();
+        });
+    
     
 });
 
-
+$(document).on('click', '.player', newButton);
 
 newButton();
+
+
 
