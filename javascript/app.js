da7849738dea8974017c25e4e7a37e29a8f0a45e
Jake Haberle <jake.haberle@gmail.com>
2020-03-08 15:01:57 -0600

    javascript and html added.  js not working as of yet

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,42 @@
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q=Dimaggio&limit=10&offset=0&rating=G&lang=en";
 
-var players = ["sandy koufax", "Babe Ruth", "Larry Walker", "Philly Fanatic", "Nolan Arendado", "Hank Aaron"];
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
+var players = [];
+
+
+
+
+function newButton() {
+
+$('#new-player').empty();
+
+for (var i = 0; i < players.length; i++) {
+    var a = $('<button>');
+    a.addClass('player');
+    a.attr('data-name', players[i]);
+    $('#new-player').append(a);
+    console.log("I cliked a button");
+}
+
+}
+
+$('search-player').on('click', function(event) {
+    event.preventDefault();
+    var player = $('#player-input').val().trim();
+    players.push(player);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+
+    
 });
 
-$('#add-player').on('click' + response)
-alert("You clicked on a button");
+
+
+newButton();
+
