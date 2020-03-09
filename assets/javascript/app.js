212cf5f91fd71b3f53e065d608115d773b507704
Jake Haberle <jake.haberle@gmail.com>
2020-03-08 22:20:59 -0600

    buttons finally working, ajax call commented out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,28 +12,31 @@ function newButton() {
         addedPlayer.text(players[i])
         $("#new-player").append(addedPlayer);
         
-    };
+    }
+}
 
 
     $("#search-player").on("click", function(event) {
         event.preventDefault();
         var player = $("#player-input").val().trim();
         players.push(player);
-
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-        $("#new-player").text(JSON.stringify(response));
         newButton();
-        });
-    })
-};   
+    });
+
+    newButton();
+        // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
+
+        // $.ajax({
+        //     url: queryURL,
+        //     method: "GET"
+        // }).then(function(response) {
+        // $("#new-player").text(response);
+        // });
+ 
+ 
 
 
-$(document).on("click", ".player", newButton);
+// $(document).on("click", ".player", newButton);
 
-newButton();
+
 
