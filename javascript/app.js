e320d03e50730e29d21400aa3ada90497560663b
Jake Haberle <jake.haberle@gmail.com>
2020-03-09 23:19:59 -0600

    updated js

--- javascript/app.js
+++ javascript/app.js
@@ -1,42 +1,45 @@
 var players = ["Ichiro", "Barry Bonds", "Babe Ruth"];
 
-$("#player-input").on("click", function(){
-    alert("you're pressing the search button")
-});
+    // $("#player-input").on("click", function(){
+    //     alert("you're pressing the search button")
+    // });
 
-function newButton() {
+    function newButton() {
     
-    $("#new-player").empty();
-    for (var i = 0; i < players.length; i++) {
-        var addedPlayer = $("<button>");
-        addedPlayer.addClass("player");
-        addedPlayer.attr("data-name", players[i]);
-        $("#new-player").append(addedPlayer);
-        
+        $("#new-player").empty();
+        for (var i = 0; i < players.length; i++) {
+            var addedPlayer = $("<button>");
+            addedPlayer.addClass("player");
+            addedPlayer.attr("data-name", players[i]);
+            $("#new-player").append(addedPlayer);
+        }
     }
-}
 
-    $("#search-player").on("click", function(event) {
-        event.preventDefault();
-        var player = $("#player-input").val().trim();
-        players.push(player);
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
+    function displayGif() {
+    // $("#search-player").on("click", function(event) {
+    //     event.preventDefault();
+        var player = $(this).attr("#player-input");
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q=" + player[i] + "&limit=10&offset=0&rating=G&lang=en";
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-        $("#new-player").text(JSON.stringify(response));
-        newButton();
+            var playerGif = $("<div class='play-gif'");
+            var gif = $("<img>").attr("src", queryURL);
+            playerGif.append(gif);
+        $("#new-player").prepend(playerGif);
+    
         });
-    
-    
-});
+    }
 
-$(document).on("click", ".player", newButton);
+
+
+$(document).on("click", ".player", displayGif) 
 
 newButton();
 
 
-
+    
