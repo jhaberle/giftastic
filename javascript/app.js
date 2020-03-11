38ffb6d5f1ad774c8f1dce53aa425aa1a925682f
Jake Haberle <jake.haberle@gmail.com>
2020-03-10 20:40:37 -0600

    button changes and adjustments made to ajax call

--- javascript/app.js
+++ javascript/app.js
@@ -1,50 +1,43 @@
 var players = ["Ichiro", "Barry Bonds", "Babe Ruth"];
 
-    // $("#player-input").on("click", function(){
-    //     alert("you're pressing the search button")
-    // });
 
-    function newButton() {
+
+function newButton() {
     
-        $("#new-player").empty();
-        for (var i = 0; i < players.length; i++) {
-            var addedPlayer = $("<button>");
-            addedPlayer.addClass("player");
-            addedPlayer.attr("data-name", players[i]);
-            $("#new-player").append(addedPlayer);
-            
-
-        }
+    $("#new-player").empty();
+    for (var i = 0; i < players.length; i++) {
+        var addedPlayer = $("<button>");
+        addedPlayer.addClass("player");
+        addedPlayer.attr("data-name", players[i]);
+        addedPlayer.text(players[i])
+        $("#new-player").append(addedPlayer);
         
     }
+}
 
 
-
-    function displayGif() {
-    // $("#search-player").on("click", function(event) {
-    //     event.preventDefault();
-        var player = $(this).attr("#player-input");
-        
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q=" + player + "&limit=10&offset=0&rating=G&lang=en";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            var playerGif = $("<div class='play-gif'");
-            var gif = $("<img>").attr("src", queryURL);
-            playerGif.append(gif);
-        $("#new-player").prepend(playerGif);
-    
+    $("#search-player").on("click", function(event) {
+        event.preventDefault();
+        var player = $("#player-input").val().trim();
+        players.push(player);
         newButton();
-        });
+    });
+
+    newButton();
+
+    function gifCall() {
+        
+        $("#search-player").on("click", function(call) {
+            call.preventDefault()
+            var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=cEm2R8y7shniOVf8cxDEDp6nusYySg89&q="+ players + "&limit=10&offset=0&rating=G&lang=en";
+        
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+            console.log(response);
+            });
+        })
     }
 
-
-
-$(document).on("click", ".player", displayGif) 
-
-newButton();
-
-
-    
+ 
